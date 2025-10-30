adc95325d18710ca3116d84f86814c0c854f64b5
marialobatsevych <marialobatsevych@gmail.com>
2025-10-30 15:37:31 +0200

    Fix capacitor.config.ts and prepare for Codemagic build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marialobatsevych.papi',
+  appName: 'Papi Steps',
+  webDir: 'dist'
+};
+
+export default config;
